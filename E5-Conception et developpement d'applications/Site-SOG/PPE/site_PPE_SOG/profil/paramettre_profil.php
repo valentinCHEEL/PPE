262dd6107b37458aedae84bae91dd7ed4b2a46de
valentinCHEEL <156203235+valentinCHEEL@users.noreply.github.com>
2024-06-05 15:48:45 +0200

    Add files via upload

---
+++ E5-Conception et developpement d'applications/Site-SOG/PPE/site_PPE_SOG/profil/paramettre_profil.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+if ($conn->connect_error) {
+    die("La connexion a échoué: " . $conn->connect_error);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modifier les informations du client</title>
+    
+</head>
+<body>
+    <h2>Modifier les informations de <?php echo $_SESSION['nomClient']?> </h2>
+    <form action="modifier_profil.php" method="post">
+        <label for="nomClient">Nom:</label><br>
+        <input type="text" id="nomClient" name="nom" value="<?php echo htmlspecialchars($_SESSION['client']['nomClient'] ?? '', ENT_QUOTES, 'UTF-8'); ?>" required>
+
+        <label for="adresse">Adresse:</label><br>
+        <input type="text" id="adresse" name="adresse" value ="<?= $_SESSION['client']['adresse'] ?>" ><br>
+
+        <label for="ville">Ville:</label><br>
+        <input type="text" id="ville" name="ville" value ="<?= $_SESSION['client']['ville'] ?>" ><br><br>
+
+        <label for="code_postal">Code postal:</label><br>
+        <input type="number" id="code_postal" name="code_postal" value ="<?= $_SESSION['client']['codePostal'] ?>"  ><br><br>
+
+        <label for="telephone">Numero téléphone:</label><br>
+        <input type="number" id="telephone" name="telephone"value ="<?= $_SESSION['client']['numTel'] ?>" ><br><br>
+
+        <label for="adressemail">Adresse email:</label><br>
+        <input type="email" id="adressemail" name="adressemail"value ="<?= $_SESSION['client']['adressemail'] ?>" ><br><br>
+
+        <label for="passwordc">Mot de passe:</label><br>
+        <input type="password" id="passwordc" name="passwordc"value ="<?= $_SESSION['client']['passwordc'] ?>" ><br><br>
+
+        <input type="submit" value="Enregistrer les modifications">
+    </form>
+
+</body>
+</html>
